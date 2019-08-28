71e8da2f7ccbda889eb35e1efebaf4e1681b5554
Ginger <2548540761@qq.com>
2019-08-28 22:10:21 +0800

    update activity_note

---
+++ app/src/main/java/com/ginger/speednote/storage/xml/WriteInXML.java
@@ -0,0 +1,18 @@
+package com.ginger.speednote.storage.xml;
+
+public class WriteInXML {
+
+    private volatile static int Note_No = 1;
+
+    /**
+     * write note in xml
+     */
+    public void writeIn(){}
+
+    /**
+     * do encryption with XML
+     */
+    public void encryptXML(){
+
+    }
+}
